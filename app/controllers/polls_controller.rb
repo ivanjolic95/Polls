85495b96b9dbee4ce481ee6a660049441e1f0619
Ivan Jolic <ivan.jolic1995@gmail.com>
2014-03-02 15:21:34 +0100

    Added methods

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -1,2 +1,38 @@
 class PollsController < ApplicationController
+
+	before_action :set_poll, only: [:show, :edit, :update, :destroy]
+
+	def new
+		@poll = Poll.new
+	end
+
+	def create
+		@poll = Poll.new(poll_params)
+		if @poll.save
+			redirect_to @poll, notice: 'Anketa je uspjesno kreirana.'
+		else
+			render action: :new
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @poll.update(poll_params)
+			redirect_to @poll, notice: 'Anketa je uspjesno azurirana.'
+		else
+			render action: :edit
+		end
+	end
+
+	private
+	def set_poll
+		@poll = Poll.find(params[:id])
+	end
+
+	def poll_params
+		params.require(:poll).permit(:question, :visible)
+	end
+
 end
